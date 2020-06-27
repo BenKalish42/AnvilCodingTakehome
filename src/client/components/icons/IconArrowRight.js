b6a179436bc7c44073292d90f50768ec8cd836c4
Ben Kalish <bkalish@rocketvisor.com>
2020-06-26 23:41:40 -0400

    little fix for fillrule console warning in new SVG

--- src/client/components/icons/IconArrowRight.js
+++ src/client/components/icons/IconArrowRight.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const Icon = ({ fill, ...others }) => (
   <svg width="16" height="16" viewBox="0 0 16 16" {...others}>
-    <path fill-rule="evenodd" d="M8.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.793 8 8.146 5.354a.5.5 0 0 1 0-.708z" fill={fill} />
-    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5H11a.5.5 0 0 1 0 1H4.5A.5.5 0 0 1 4 8z" fill={fill} />
+    <path d="M8.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.793 8 8.146 5.354a.5.5 0 0 1 0-.708z" fill={fill} />
+    <path d="M4 8a.5.5 0 0 1 .5-.5H11a.5.5 0 0 1 0 1H4.5A.5.5 0 0 1 4 8z" fill={fill} />
   </svg>
 )
 
